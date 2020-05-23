d5e1797ab6094977ff4764f3083225e3c31609fa
cheetosysst <thect@protonmail.com>
2020-05-23 22:21:40 +0800

    Add real emoji

--- process.py
+++ process.py
@@ -1,9 +1,10 @@
 from bs4 import BeautifulSoup as bs
 import requests
 import re
+import emoji
 
 html = requests.get("https://zh.wikipedia.org/wiki/繪文字")
-soup = bs(html, "html.parser")
+soup = bs(html.text, "html.parser")
 #print(soup.prettify())
 
 out = open("emoji_list", "w")
@@ -14,7 +15,8 @@ for i in tags:
     #if i.string is not None:
     #print(i["title"])
     sp = i["title"].split(":")
-    out.write(sp[0]+sp[1]+"\n")
-    print(sp[0]+sp[1])
+    uni = sp[0].split("+")[1]
+    out.write(chr(int(uni,16))+" "+sp[0]+sp[1]+"\n")
+    print(chr(int(uni, 16)), sp[1])
 
 #print(tags[0].prettify())
