0c7dea7f3c563111f2de5d09823d67dbe65d9c73
cheetosysst <thect@protonmail.com>
2020-05-24 00:03:54 +0800

    Switch to bash
    Now you can run the script anywhere you want

--- demoji.sh
+++ demoji.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 # Inspired by Luke Smith's video "https://youtu.be/UCEXY46t3OA"
-
-cat emoji_list | dmenu -i -l 20 -fn Monospace-14 | awk '{print $1}u' | tr -d '\n' | xclip -selection clipboard
+BASEDIR=$(dirname "$0")
+cat $BASEDIR/emoji_list | dmenu -i -l 20 -fn Monospace-14 | awk '{print $1}u' | tr -d '\n' | xclip -selection clipboard
 pgrep -x dunst > /dev/null && notify-send "$(xclip -o -selection clipboard) 已複製到剪貼簿"
